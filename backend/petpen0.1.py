6f0392e2fa077f29c9da61266f5783fba76f6920
Chia-Yu Tsai <jimisdaisy@gmail.com>
2017-09-07 16:04:10 +0800

    modify callbacks

--- backend/petpen0.1.py
+++ backend/petpen0.1.py
@@ -1,80 +1,64 @@
 import argparse
 import time
+from datetime import datetime
 import sys,os,re
 import pandas as pd
 
 from model import get_model,backend_model
-from data_provider import Demo1,Mnist
 from utils import build_model_command
-from utils import get_command,Batch_History,Model_state,load_dataset
+from utils import Batch_History,Model_state,load_dataset
 from utils import save_history
-from keras.callbacks import TensorBoard
+import utils
+# from keras.callbacks import TensorBoard
 
 parser = argparse.ArgumentParser(prog='petpen')
 subparsers = parser.add_subparsers()
+parser.add_argument('-n', '--name', default='result', help='specify the model name.')
+parser.add_argument('-m', '--model', required=True, help='set model folder')
+parser.add_argument('-d', '--dataset', required=True, help='set dataset folder')
+parser.add_argument('-w', dest='w',action='store_true', help='set -w to load weight')
+parser.set_defaults(w=False)
+
 subparser = subparsers.add_parser('model')
-subparser.add_argument('-n', '--name', default='result', help='specify the model name.')
-subparser.add_argument('-m', '--model', required=True, help='set model folder')
-subparser.add_argument('-d', '--dataset', required=True, help='set dataset folder')
 subparser.set_defaults(func=build_model_command)
-parser.add_argument('-w', default='False', help='set -w True to load weight')
+
+subparser = subparsers.add_parser('train')
+subparser.set_defaults(func=build_model_command)
+
+subparser = subparsers.add_parser('validate')
+subparser.set_defaults(func=build_model_command)
+
+subparser = subparsers.add_parser('predict')
+subparser.set_defaults(func=build_model_command)
 
 args = parser.parse_args()
 model,model_dir = args.func(args)
 
-# (x_train,y_train),(x_test,y_test) = data.load_dataset(args.d,model.features)
-
-if args.name == 'demo1':
-    data = Demo1()
-elif args.name == 'mnist' or 'mnist_cnn':
-    data = Mnist()
-(x_train,y_train),(x_test,y_test) = data.load_data()
-
-
 b_history = Batch_History()
-trainlog_path = os.path.join(model_dir,'logs')
-if not os.path.exists(trainlog_path):
-    os.mkdir(trainlog_path)
+str_start_time = datetime.now().strftime('%y%m%d_%H%M%S')
+model_result_path = os.path.join(model_dir, str_start_time)
+os.mkdir(model_result_path)
+
+trainlog_dir = os.path.join(model_result_path,'logs')
+os.mkdir(trainlog_dir)
 
 state_file = os.path.join('/home/plash/petpen/','state.json')
 state = Model_state(state_file,model.config)
 
-history = model.train(x_train,y_train,callbacks = [b_history,state])
-trainlog_name = 'train_log'#+'_'+str(int(time.time()))
-pd.DataFrame(history.history).to_csv(os.path.join(trainlog_path,trainlog_name),index=False)
-pd.DataFrame(b_history.history).to_csv(os.path.join(trainlog_path,trainlog_name+'full'),index=False)
+if args.w:
+    record_files = [f for f in os.listdir(model_dir) if re.match(r'\d{6}_\d{6}',f)]
+    if not record_files:
+        print('no saved weight found.')
+    else:
+        weight_file = os.path.join(max(record_files),'weights.h5')
+    if os.path.exists(weight_file):
+        model.load_weights(weight_file)
 
-model.save_weights('weights.h5')
-print(model.model.layers[1].weights)
+history = model.train(callbacks = [b_history,state])
+save_history(os.path.join(trainlog_dir,'train_log'),history,b_history)
+model.save_weights(os.path.join(model_result_path,'weights.h5'))
 
 time.sleep(1)
-model.evaluate(x_test,y_test)
-
-trainlog_name = 'test_log'#+'_'+str(int(time.time()))
-pd.DataFrame(history.history).to_csv(os.path.join(trainlog_path,trainlog_name),index=False)
-pd.DataFrame(b_history.history).to_csv(os.path.join(trainlog_path,trainlog_name+'full'),index=False)
-
-# y_test = model.predict(x_test)
-# pd.DataFrame(y_test).to_csv(os.path.join(trainlog_path,'result.csv'),index=False)
-
-
-# while True:
-    # cmd = get_command()
-    # print(cmd)
-    # if cmd == 'exit':
-        # break
-    # if cmd not in ['train','predict']:
-        # continue
-    # if cmd == 'train':
-        # history = model.train(x_train,y_train,callbacks = [b_history,state])
-        # trainlog_name = 'train_log'#+'_'+str(int(time.time()))
-        # pd.DataFrame(history.history).to_csv(os.path.join(trainlog_path,trainlog_name),index=False)
-        # pd.DataFrame(b_history.history).to_csv(os.path.join(trainlog_path,trainlog_name+'full'),index=False)
-
-    # if cmd == 'predict':
-        # # model.evaluate(x_test,y_test)
-        # y_test = model.predict(x_test)
-        # pd.DataFrame(y_test).to_csv(os.path.join(trainlog_path,'result.csv'),index=False)
-
-# if __name__ = '__main__':
+model.evaluate()
+save_history(os.path.join(trainlog_dir,'validate_log'),history,b_history)
 
