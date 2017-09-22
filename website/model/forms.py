680f508482f91380da194e9a26583bd45e54eb5f
Ming <rf37535@gmail.com>
2017-09-22 14:40:37 +0800

    add minimal dockerfile

--- website/model/forms.py
+++ website/model/forms.py
@@ -12,8 +12,9 @@ class UploadFileForm(forms.Form):
     )
 
 class DatasetForm(forms.Form):
-    dataset_choices = [('test','test')]+[(dataset.title, dataset.title) for dataset in Dataset.objects.all()]
-    dataset = forms.ChoiceField(choices=dataset_choices, widget=forms.Select(attrs={'id':'select_dataset','name':'sel_data','onchange':'this.form.submit();'}))
+    #dataset_choices = [('test','test')]+[(dataset.title, dataset.title) for dataset in Dataset.objects.all()]
+    #dataset = forms.ChoiceField(choices=dataset_choices, widget=forms.Select(attrs={'id':'select_dataset','name':'sel_data','onchange':'this.form.submit();'}))
+    pass
 
 class FeatureForm(forms.Form):
     input_features = forms.MultipleChoiceField()
