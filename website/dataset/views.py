34bda4d0cb810e1907da326c5bf2ad07ded194b3
Chia-Yu Tsai <jimisdaisy@gmail.com>
2017-10-05 11:44:21 +0800

    autoload nodered & modify dataset page

--- website/dataset/views.py
+++ website/dataset/views.py
@@ -10,17 +10,13 @@ MEDIA_ROOT = '/media/disk1/petpen/datasets'
 def index(request):
     if not request.user.is_authenticated():
         return HttpResponseRedirect('/login/')
-    form = UploadFileForm()
-    datasets = Dataset.objects.all()
-    users = User.objects.all()
-    return render(request, 'dataset/index.html', {'datasets':datasets,'form':form})
     
     if request.method == 'POST':
         if 'delete-dataset' in request.POST:
             form = UploadFileForm()
             Dataset.objects.get(id=request.POST['delete-dataset']).delete()
         else:
-            if request.POST['shared_testing']:
+            if request.POST.get('shared_testing',False):
                 request.FILES['testing_csvfile'] = request.FILES['training_csvfile']
             form = UploadFileForm(request.POST, request.FILES)
             if form.is_valid():
@@ -29,15 +25,15 @@ def index(request):
                 return HttpResponseRedirect(reverse("index"))
     else:
         form = UploadFileForm()
-    datasets = Dataset.objects.all()
+    datasets = Dataset.objects.filter(user_id = request.user.id)
     return render(request, 'dataset/index.html', {'datasets':datasets, 'form':form})
 
 def dataset_detail(request, dataset_id):
     dataset = get_object_or_404(Dataset, pk=dataset_id)
-    context = dataset
-    # dataset_file = dataset.csvfile.open()
-    # dataset_name = dataset.csvfile.name
-    # import pandas as pd
-    # dataframe = pd.read_csv(MEDIA_ROOT+dataset_name)
-    return render(request,'dataset/nav.html',{})
+    context = {}
+    import pandas as pd
+    training_dataset = pd.read_csv(dataset.training_csvfile)
+    context['features'] = list(training_dataset.columns)
+    context['dataset'] = dataset
+    return render(request,'dataset/dataset.html',context)
 
--- website/model/urls.py
+++ website/model/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     url(r'^results/$', views.results, name='results'),
     url(r'^api/parse/$',views.api, name='api'),
     url(r'^api/plot/$',views.plot_api, name='plot_api'),
+    url(r'^api/close-nodered/$', views.closenodered, name='nodered_api')
     # url(r'^configure/', views.configure, name='configure')
 ]
