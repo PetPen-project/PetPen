f32bb72f5ee980fdde6d2be5efe169ffc4cbe73f
kazami <kazamizero@gmail.com>
2017-10-19 11:47:39 +0800

    merge

--- Output_node/outputNode/output.js
+++ Output_node/outputNode/output.js
@@ -3,11 +3,18 @@ module.exports = function(RED) {
         RED.nodes.createNode(this,config);
 	var node = this;
 	this.on('input', function(msg) {
+
 	var fs = require('fs');
+	console.log("ff")
+	if (fs.existsSync('flows_plash-SYS-7048GR-TR.json')) {
 	var obj = JSON.parse(fs.readFileSync('flows_plash-SYS-7048GR-TR.json', 'utf-8'));
+	} else {
+	  return;
+	}
 	var res = {
 		layers: {},
 		connections: {}, 
+		dataset: {},
 	};
         var rec = {};
 	for (var ind in obj) {
@@ -26,22 +33,42 @@ module.exports = function(RED) {
 	    		for (var i = 0; i < stride_num.length; i++) {
 				stride_num[i] = parseInt(stride_num[i]);
 	    		}
-			res.layers[name] = {type: 'CONVOLUTION_2D', params: {filters: parseInt(obj[i]['filters']), kernel_size: kernels, strides: parseInt(stride_num), activation: obj[ind]['methods']}};
+			res.layers[name] = {type: 'CONVOLUTION_2D', params: {filters: parseInt(obj[ind]['filters']), kernel_size: kernels, strides: parseInt(stride_num), activation: obj[ind]['activation']}};
+			rec[obj[ind]['id']] = name;
+            		obj[ind]['name'] = name;
+        	} else if (obj[ind]['type'] == 'ConvolutionLSTM_2D') {
+			name  = obj[ind]['name'] + "_" + ind;
+            		kernels = obj[ind]['kernel'].split(",");
+	    		for (var i = 0; i < kernels.length; i++) {
+				kernels[i] = parseInt(kernels[i]);
+	    		}
+            		stride_num = obj[ind]['strides'].split(",");
+	    		for (var i = 0; i < stride_num.length; i++) {
+				stride_num[i] = parseInt(stride_num[i]);
+	    		}
+			res.layers[name] = {type: 'CONVOLULSTMTION_2D', params: {filters: parseInt(obj[ind]['filters']), kernel_size: kernels, strides: parseInt(stride_num), activation: obj[ind]['activation'], padding: obj[ind]['padding'], recurrent_activation: obj[ind]['recurrent_activation'], return_response: obj[ind]['return_response'], dropout: obj[ind]['dropout'], recurrent_dropout: obj[ind]['recurrent_dropout']}};
 			rec[obj[ind]['id']] = name;
             		obj[ind]['name'] = name;
 		} else if (obj[ind]['type'] == 'Output') {
 			name = obj[ind]['name'] + "_" + ind;
+			features = obj[ind]['feature'].split(',');
 			res.layers[name] = {type: 'Output', params: {loss: obj[ind]['loss'], optimizer: obj[ind]['optimizer'], epoch: parseInt(obj[ind]['epoch']), batchsize: parseInt(obj[ind]['batchsize'])}};
+			res.dataset[name] = features;
+			var fs = require('fs')
+			filename = fs.readFileSync('./file.name')
+			res.dataset['output'] = filename;
 			rec[obj[ind]['id']] = name;
             obj[ind]['name'] = name;
 		} else if (obj[ind]['type'] === 'Input') {
-			console.log(rec);
+			console.log(obj[ind]);
 			name = obj[ind]['name'] + "_" + ind;
             		shapes = obj[ind]['shape'].split(',');
+			features = obj[ind]['feature'].split(',');
 			for (var ii = 0; ii < shapes.length; ii++) {
 				shapes[ii] = parseInt(shapes[ii]);
 			}
 			res.layers[name] = {type: 'Input', params: {shape: shapes}}; 
+			res.dataset[name] = features;
 			rec[obj[ind]['id']] = name;
             		obj[ind]['name'] = name;
 		} else if (obj[ind]['type'] == 'Dense') {
@@ -59,9 +86,23 @@ module.exports = function(RED) {
 			res.layers[name] = {type: 'Reshape', params: {shape: shapes}}; 
 			rec[obj[ind]['id']] = name;
             obj[ind]['name'] = name;
+	} else if (obj[ind]['type'] == 'MaxPooling2D') {
+		name = obj[ind]['name'] + "_" + ind;
+		pool_size_arr = obj[ind]['poolsize'].split(',');
+		for (var i = 0; i < pool_size_arr.length; i++) {
+			pool_size_arr[i] = parseInt(pool_size_arr[i]);
+		}
+		strides_arr = obj[ind]['strides'].split(',');
+		for (var i = 0; i < strides_arr.length; i++) {
+			strides_arr[i] = parseInt(strides_arr[i]);
+		}
+		res.layers[name] = {type: 'MaxPooling2D', params: {strides: strides_arr, pool_size: pool_size_arr, padding: obj[ind]['padding']}};
+		rec[obj[ind]['id']] = name;
+		obj[ind]['name'] = name;
+
         } else if (obj[ind]['type'] == 'Merge') {
 			name = obj[ind]['name'] + "_" + ind;
-			res.layers[name] = {type: 'Merge', params: {activation: obj[i]['activation']}}; 
+			res.layers[name] = {type: 'Merge', params: {activation: obj[ind]['method']}}; 
 			rec[obj[ind]['id']] = name;
             obj[ind]['name'] = name;
         } else if (obj[ind]['type'] == 'SimpleRNN') {
@@ -84,14 +125,18 @@ module.exports = function(RED) {
 			res.layers[name] = {type: 'Gru', params: {units: parseInt(obj[ind]['units']), activation: obj[ind]['activation']}}; 
 			rec[obj[ind]['id']] = name;
             obj[ind]['name'] = name;
-        }
-
+        } else if (obj[ind]['type'] == 'File') {
+		path = obj[ind]['file'];
+		//var fst = require('fs');
+		//var content = fst.readFileSync(path, 'utf-8')
+		//fst.writeFileSync("./data.csv", content, 'utf8');
+		console.log("myfile");
+		console.log(path);
+	}
          
 
 	}
-	console.log(obj);
-	console.log(res);
-	console.log(rec);
+	console.log("now");
 	for (var i in obj) {
 		name = obj[i]['name'];
 		if (obj[i]['wires'] == null) {
@@ -112,20 +157,51 @@ module.exports = function(RED) {
 	json = JSON.stringify(res);
 //      fs.writeFileSync('/home/kazami/.node-red/result.json', json, 'utf-8');
         fs.writeFileSync('result.json', json, 'utf-8');
-/*	var exec = require('child_process').exec;
-	var arg1 = "5";
-	var arg2 = "6";
-	exec('python2.7 py_test.py ' + arg1 + ' ' + arg2 + ' ', function(error, stdout, stderr) {
+	var fs = require('fs');
+	var obj = fs.readFileSync('child.json', 'utf-8');
+	/*var kill = function(pid, signal, callback) {
+	  signal = signal || 'SIGKILL';
+	  callback = callback || function() {};
+	  var killTree = true;
+	  if (killTree) {
+	    psTree(pid, function(err, children) {
+	      [pid].concat(
+		  children.map(function (p) {
+		    return p.PID;
+		  })
+		).forEach(function (tpid) {
+		    try { process.kill(tpid, signal)}
+		    catch (ex) {}
+		});
+		callback();
+	    });
+	  } else {
+	    try { process.kill(pid, signal) }
+	    catch (ex) { }
+	    callback();
+	  }
+	};
+	kill(previous_pid);*/
+	console.log(obj)
+	if (obj >= 1) {
+	var exec = require('child_process').exec;
+	var arg1 = "-d . ";
+	var arg2 = "-n demo1";
+	var newproc = exec('python /home/plash/petpen/develop/petpen0.1.py ' + arg1 + ' ' + arg2 + ' ', function(error, stdout, stderr) {
 		if (stdout.length > 1) {
 			console.log('offer', stdout);
 		} else {
 			console.log('don\'t offer');
 		}
 	});
-*/	
+	console.log(newproc.pid);
+      fs.writeFileSync('pid.json', newproc.pid, 'utf-8');
+	}
+	
         node.send(msg);
         
 	});
+
     }
     RED.nodes.registerType("Output",output_go);
 };
---
+++ node-red-contrib-file-upload/16-file-upload.js
@@ -0,0 +1,56 @@
+/**
+ * Copyright 2013 IBM Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+module.exports = function(RED) {
+  'use strict';
+  var poster = require('poster');
+
+  function FileUploadNode(n) {
+    RED.nodes.createNode(this, n);
+    this.name = n.name;
+    this.url = n.url;
+    this.file = n.file;
+    this.func = n.func;
+    var functionText = 'var results = (function(msg){' + this.func + '\n})(msg);';
+    this.topic = n.topic;
+    var _this = this;
+
+    this.on('input', function(msg) {
+
+      var options = {
+        uploadUrl: n.url,
+        method: 'POST',
+        fileId: 'file',
+        fields: {
+          'script': '{"file":"%s"}'
+        },
+        uploadHeaders:{
+          'Cookie':((msg.cookie)? msg.cookie : "")
+        }
+      };
+
+      poster.post(n.file, options, function(err, data) {
+        if (!err) {
+          msg.payload = data;
+          _this.send(msg);
+        } else {
+          _this.error(err);
+        }
+      });
+    });
+  }
+  RED.nodes.registerType('file-upload', FileUploadNode);
+}
